cd26416f30548589fc0ad3a652e79da04f1b95c7
Kiran <Kpatle311@gmail.com>
2023-04-25 13:42:50 +0530

    Refacore the code

--- controllers/Blog_Post_Controller.py
+++ controllers/Blog_Post_Controller.py
@@ -5,15 +5,21 @@ from starlette.authentication import requires
 from typing import List
 from data.blog_post_data import BlogPostDataProvider
 
-serialized_posts =BlogPostDataProvider.seed_blog_posts()
 
 @requires("authenticated")
 async def get_user_posts(request: Request):
+    """This method is responsible to return user blog post data with/without user_id
+
+    Args:
+        request (Request): This is starlette request object of http request type.
+
+    Returns:
+        _type_: This return jsonresponse for blog post data given user id or response for all users if user id not passed
+    """
     user_id = request.query_params.get('user_id')
+    serialized_posts =BlogPostDataProvider.seed_blog_posts()
     if user_id:
         blogResults =  [post for post in serialized_posts if post["user_id"] == int(user_id)]
-        #result  = json.dumps(blogResults)
     else:
-        #result = json.dumps(serialized_posts)
         blogResults = serialized_posts
     return JSONResponse(content=blogResults)
